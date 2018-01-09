07e4cd3a95959a58700ec34454586de4c7378d8a
Nick Wilson <thenickwilson@gmail.com>
2018-01-09 15:32:17 +0000

    Use property testing for question form

--- src/main/scaffolds/questionPage/generated-test/forms/$className$FormProviderSpec.scala
+++ src/main/scaffolds/questionPage/generated-test/forms/$className$FormProviderSpec.scala
@@ -1,23 +1,45 @@
 package forms
 
-import forms.behaviours.FormBehaviours
-import models.{Field, $className$, Required}
+import forms.behaviours.FieldBehaviours
+import play.api.data.FormError
 
-class $className$FormProviderSpec extends FormBehaviours {
-
-  val validData: Map[String, String] = Map(
-    "field1" -> "value 1",
-    "field2" -> "value 2"
-  )
+class $className$FormProviderSpec extends FieldBehaviours {
 
   val form = new $className$FormProvider()()
 
-  "$className$ form" must {
-    behave like questionForm($className$("value 1", "value 2"))
+  ".field1" must {
 
-    behave like formWithMandatoryTextFields(
-      Field("field1", Required -> "$className;format="decap"$.error.field1.required"),
-      Field("field2", Required -> "$className;format="decap"$.error.field2.required")
+    val fieldName = "field1"
+    val requiredKey = "$className;format="decap"$.error.field1.required"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      nonEmptyString
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+
+  ".field2" must {
+
+    val fieldName = "field2"
+    val requiredKey = "$className;format="decap"$.error.field2.required"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      nonEmptyString
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
     )
   }
 }
